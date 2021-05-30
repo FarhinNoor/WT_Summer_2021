f114843f6505507b7324ab7ffbf59d7b2b72b535
FarhinNoor <84770304+FarhinNoor@users.noreply.github.com>
2021-05-30 15:53:02 +0600

    Add files via upload

---
+++ LabTask1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Registration Form</title>
+</head>
+<body>
+ <form>
+  <table>
+   <tr>
+    <td>Name :</td>
+    <td><input type="text"></td>
+   </tr>
+   <tr>
+    <td>Password :</td>
+    <td><input type="password"></td>
+   </tr>
+   <tr>
+    <td>Gender :</td>
+    <td>
+     <input type="radio" name="Gender"> Male
+     <input type="radio" name="Gender"> Female
+    </td>
+   </tr>
+   <tr>
+    <td>Email :</td>
+    <td><input type="email"></td>
+   </tr>
+   <tr>
+    <td>Phone no :</td>
+    <td>
+     <select>
+      <option>123</option>
+      <option>456</option>
+      <option>789</option>
+      <option>112</option>
+      <option>113</option>
+      <option>974</option>
+     </select>
+     <input type="phone">
+    </td>
+   </tr>
+   <tr>
+    <td><input type="submit" value="Submit"></td>
+   </tr>
+  </table>
+ </form>
+</body>
+</html>
